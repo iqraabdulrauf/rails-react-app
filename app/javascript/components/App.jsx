c5b3768858aa1100a11a464fa87b93cbcb9d3d61
iqra abdul rauf <iqraabdulrauf01@gmail.com>
2021-08-30 17:04:49 +0500

    material form added

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,29 +1,72 @@
 import React, { Component } from "react";
-import { TextField ,Button} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  TextField,
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Input,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
 
 export default class App extends Component {
-    
   render() {
     return (
       <>
-       <form  noValidate autoComplete="off">
-       <h1>Hello world, this is your app speaking</h1>
-       <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
-       <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-       </form>
-        
+        <h1>Welcome to our first rails app with react</h1>
+
+        <FormControl>
+          <TextField
+            id="standard-full-width"
+            label="Name"
+            style={{ margin: 8 }}
+            placeholder=""
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+
+          <TextField
+            id="standard-full-width"
+            label="Company"
+            style={{ margin: 8 }}
+            placeholder=""
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+
+          <TextField
+            id="standard-full-width"
+            label="Email address"
+            style={{ margin: 8 }}
+            placeholder=""
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+
+          <br></br>
+          <br></br>
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </FormControl>
       </>
     );
   }
